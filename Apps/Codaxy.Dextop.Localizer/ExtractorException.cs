de91a4b550182cd4dcd77167aa6b0a46e283d176
Bajic Dusko <bajicdusko@gmail.com>
2016-02-15 11:13:40 +0100

    Detailed extractor exception.

---
+++ Apps/Codaxy.Dextop.Localizer/ExtractorException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Codaxy.Dextop.Localizer
+{
+    public class ExtractorException : Exception
+    {
+        public ExtractorException() : base() { }
+        public ExtractorException(string message) : base(message) { }
+        public ExtractorException(string message, params object[] args) : base(String.Format(message, args)) { }
+        public ExtractorException(Exception innerException, string message, params object[] args) : base(String.Format(message, args), innerException) { }
+    }
+}
